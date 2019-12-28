f19c50b6941f5fee96ee57ee909a1ee8a7b215f8
Paul Goddard <paul2.goddard@live.uwe.ac.uk>
2019-12-28 19:43:24 +0000

    some NFC to database testing

--- db.cpp
+++ db.cpp
@@ -1,45 +1,116 @@
-#include <iostream> 
-#include <sqlite3.h> 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <iostream>
 
-using namespace std; 
+#include <sqlite3.h>
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
 
-static int callback(void* data, int argc, char** argv, char** azColName) 
-{ 
-	int i; 
-	fprintf(stderr, "%s: ", (const char*)data); 
+#include "RC522.c"
 
-	for (i = 0; i < argc; i++) { 
-		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL"); 
-	} 
+static int callback(void* data, int argc, char** argv, char** azColName) {
+	int i;
+	fprintf(stderr, "%s: ", (const char*)data);
 
-	printf("\n"); 
-	return 0; 
-} 
+	for (i = 0;i < argc;i++) {
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
 
-int main(int argc, char** argv) 
-{ 
-	sqlite3* DB; 
-	int exit = 0; 
-	exit = sqlite3_open("test.db", &DB); 
-	string data("CALLBACK FUNCTION"); 
+	printf("\n");
+	return 0;
+}
 
-	string sql("SELECT * FROM sensors;"); 
-	if (exit) { 
-		std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl; 
-		return (-1); 
-	} 
-	else
-		std::cout << "Opened Database Successfully!" << std::endl; 
+std::string get_nfc_ID(){
+	char cStr [ 30 ];
+	uint8_t ucArray_ID [ 4 ];    /*先后存放IC卡的类型和UID(IC卡序列号)*/
+	uint8_t ucStatusReturn;      /*返回状态*/
+	static uint8_t ucLineCount = 0;
+	while ( 1 ) {
+		/*寻卡*/
+		if ( ( ucStatusReturn = PcdRequest ( PICC_REQALL, ucArray_ID ) ) != MI_OK ) { /*若失败再次寻卡*/
+			ucStatusReturn = PcdRequest ( PICC_REQALL, ucArray_ID );
+		}
 
-	int rc = sqlite3_exec(DB, sql.c_str(), callback, (void*)data.c_str(), NULL); 
+		if ( ucStatusReturn == MI_OK  ) {
+			/*防冲撞（当有多张卡进入读写器操作范围时，防冲突机制会从其中选择一张进行操作）*/
+			if ( PcdAnticoll ( ucArray_ID ) == MI_OK ) {
+				sprintf ( cStr, "%02X%02X%02X%02X",
+				          ucArray_ID [ 0 ],
+				          ucArray_ID [ 1 ],
+				          ucArray_ID [ 2 ],
+				          ucArray_ID [ 3 ] );
+				return cStr;
 
-	if (rc != SQLITE_OK) 
-		cerr << "Error SELECT" << endl; 
-	else { 
-		cout << "Operation OK!" << endl; 
-	} 
+				//not needed anymore
+				// ucLineCount ++;
+				// if ( ucLineCount == 17 ) {
+					// ucLineCount = 0;
+				// }
+				// return cStr;
+			}
+		}
+	}
+}
 
-	sqlite3_close(DB); 
-	return (0); 
-} 
+int main(int argc, char** argv) {
+	sqlite3* DB;
+	char* messaggeError;
+	int exit = sqlite3_open("audio_culture.db", &DB);
+	std::string query = "SELECT * FROM tag;";
 
+	std::cout << "STATE OF TABLE BEFORE INSERT" << std::endl;
+
+	sqlite3_exec(DB, query.c_str(), callback, NULL, NULL);
+
+	RC522_setup(7);
+	PcdReset ();
+	M500PcdConfigISOType('A');
+	printf ( "Start NFC scan\r\n" );
+	while(1) {
+		std::string nfcID = "";
+		nfcID = get_nfc_ID();
+		if(nfcID.length() > 0) {
+			std::cout << "Tag successfully scanned: " << nfcID << " ... searching for matching record..." << std::endl;
+
+			//insert tag ID into database:
+			std::string sql("SELECT desc FROM tag WHERE ID = '"+nfcID+"';");
+			if (exit) {
+				std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+				return (-1);
+			}
+			// else
+				// std::cout << "Opened Database Successfully!" << std::endl;
+
+			int rc = sqlite3_exec(DB, sql.c_str(), callback, NULL, NULL);
+
+			if (rc != SQLITE_OK)
+				std::cerr << "Error SELECT" << std::endl;
+			// else {
+				// std::cout << "Query successful (not that the results are positive)" << std::endl;
+			// }
+
+
+			////insert tag ID into database:
+			// std::string sql_ins("INSERT INTO tag VALUES('"+nfcID+"','desc');");
+
+			// exit = sqlite3_exec(DB, sql_ins.c_str(), NULL, 0, &messaggeError);
+			// if (exit != SQLITE_OK) {
+				// std::cerr << "Error Insert" << std::endl;
+				// sqlite3_free(messaggeError);
+			// }
+			// else
+				// std::cout << "Records created Successfully!" << std::endl;
+
+			// std::cout << "STATE OF TABLE AFTER INSERT" << std::endl;
+
+			// sqlite3_exec(DB, query.c_str(), callback, NULL, NULL);
+			
+		}
+		delay(100);
+	}
+
+	sqlite3_close(DB);
+	return (0);
+}
